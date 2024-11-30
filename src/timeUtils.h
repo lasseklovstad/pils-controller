57ddad88714e8c2916338017efcd8dbe9c1b2c24
Lasse Narula <lasse.klovstad@gmail.com>
2024-11-30 20:01:25 +0100

    Add nfnfdfd

--- src/timeUtils.h
+++ src/timeUtils.h
@@ -3,6 +3,11 @@
 #include <Arduino.h>
 #include <time.h>
 
+#ifdef UNIT_TEST
+// Declare the mock configTime function for unit tests
+void configTime(long gmtOffset_sec, int daylightOffset_sec, const char* server1, const char* server2 = nullptr, const char* server3 = nullptr);
+#endif
+
 void configureTime();
 
 time_t getTimeStamp();
