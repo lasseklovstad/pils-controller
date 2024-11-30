11279728cfe19b6e0c7cd5aa454eb04c7aeba399
Lasse Narula <lasse.klovstad@gmail.com>
2024-11-30 20:26:58 +0100

    Halla

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -7,7 +7,7 @@ void test_example(void) {
     TEST_ASSERT_EQUAL(1, 1);
 }
 
-void test_happy_case_shouldTurnOnSource(void){
+void test_happy_case_warm_active_shouldTurnOnSource(void){
     Controller controller(1, "secret");
     controller.update("warm", "active", "1000-18.0;2000-15.0;4000-25.3");
     controller.setTemperature(18.2);
@@ -19,15 +19,31 @@ void test_happy_case_shouldTurnOnSource(void){
     TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(5000), true);
 }
 
+void test_happy_case_cold_active_shouldTurnOnSource(void){
+    Controller controller(1, "secret");
+    controller.update("cold", "active", "1000-18.0;2000-15.0;4000-25.3");
+    controller.setTemperature(18.2);
+
+    TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(0), true);
+    TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(1500), true);
+    TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(2000), true);
+    TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(4000), false);
+    TEST_ASSERT_EQUAL(controller.shouldTurnOnSource(5000), false);
+}
+
 void setup() {
     UNITY_BEGIN();
     RUN_TEST(test_example);
+    RUN_TEST(test_happy_case_warm_active_shouldTurnOnSource);
+    RUN_TEST(test_happy_case_cold_active_shouldTurnOnSource);
     UNITY_END();
 }
 
 int runUnityTests(void) {
   UNITY_BEGIN();
   RUN_TEST(test_example);
+  RUN_TEST(test_happy_case_warm_active_shouldTurnOnSource);
+  RUN_TEST(test_happy_case_cold_active_shouldTurnOnSource);
   return UNITY_END();
 }
 
