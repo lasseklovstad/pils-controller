35c01ed7e0809cb736e71c08d87ecb052660f4de
Lasse Narula <lasse.klovstad@gmail.com>
2024-11-30 19:47:09 +0100

    mamma

--- test/mocks/time.h
+++ test/mocks/time.h
@@ -1,10 +1,7 @@
 #ifndef TIME_UTILS_H
 #define TIME_UTILS_H
 #include <Arduino.h>
-
-void configureTime() {};
-
-time_t getTimeStamp() { return 1000; };
+#include <time.h>
 
 void configTime(long gmtOffset_sec, int daylightOffset_sec, const char *server1) {};
 
