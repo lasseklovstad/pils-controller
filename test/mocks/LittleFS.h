2362d19836a8d8404d4c2fec6090d2cb4d3c04fb
laklov <lasse.klovstad@gmail.com>
2025-05-24 13:36:39 +0200

    Add file to mocks

--- test/mocks/LittleFS.h
+++ test/mocks/LittleFS.h
@@ -2,12 +2,32 @@
 #ifndef DUMMY_LITTLEFS_H
 #define DUMMY_LITTLEFS_H
 
+#include <string>
+#include <cstddef>
+
+// Dummy File class for native builds (testing only)
+class File {
+public:
+    File() : _open(false) {}
+    operator bool() const { return _open; }
+    bool available() { return false; }
+    void close() {}
+    int printf(const char*, ...) { return 0; }
+    std::string _buffer;
+    int readStringUntil(char) { return 0; }
+    int toInt() { return 0; }
+    float toFloat() { return 0.0f; }
+    size_t size() { return 0; }
+private:
+    bool _open;
+};
+
 class LittleFSClass {
 public:
     bool begin() { return false; }
     void end() {}
     bool format() { return false; }
-    bool open(const char*, const char*) { return false; }
+    File open(const char*, const char*) { return File(); }
     // Add more dummy methods as needed for compilation
 };
 static LittleFSClass LittleFS;
