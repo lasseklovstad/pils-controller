f7496b11ff74cfbbc50c3c92e6e8d19732fae5d9
laklov <lasse.klovstad@gmail.com>
2025-05-24 13:46:41 +0200

    TRy

--- test/mocks/LittleFS.h
+++ test/mocks/LittleFS.h
@@ -1,22 +1,7 @@
 // Dummy LittleFS.h for native builds (testing only)
 #ifndef DUMMY_LITTLEFS_H
 #define DUMMY_LITTLEFS_H
-
-#include <string>
-#include <cstddef>
-
-// Minimal mock Arduino String class for testing
-class String {
-public:
-    String() : _str("") {}
-    String(const char* s) : _str(s) {}
-    String(const std::string& s) : _str(s) {}
-    operator const char*() const { return _str.c_str(); }
-    int toInt() const { return std::stoi(_str); }
-    float toFloat() const { return std::stof(_str); }
-    // Add more methods as needed
-    std::string _str;
-};
+#include <Arduino.h>
 
 // Dummy File class for native builds (testing only)
 class File {
@@ -25,10 +10,8 @@ public:
     operator bool() const { return _open; }
     bool available() { return false; }
     void close() {}
-    int printf(const char*, ...) { return 0; }
-    std::string _buffer;
-    String readStringUntil(char) { return String("0"); }
-    size_t size() { return 0; }
+    void printf(const char*, ...) { }
+    String readStringUntil(char) { return String(""); }
 private:
     bool _open;
 };
@@ -38,8 +21,7 @@ public:
     bool begin() { return false; }
     void end() {}
     bool format() { return false; }
-    File open(const char* /*filename*/, const char* /*mode*/) { return File(); }
-    File open(const String& /*filename*/, const char* /*mode*/) { return File(); }
+    File open(const char*, const char*) { return File(); }
     // Add more dummy methods as needed for compilation
 };
 static LittleFSClass LittleFS;
